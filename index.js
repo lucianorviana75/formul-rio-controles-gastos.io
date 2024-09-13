139c4927a0339fe99e558613a219b9008d4784eb
luciano <ribeirovianaluciano98@gmail.com>
2024-09-13 17:04:55 -0300

    4°etapa mensagens de erro

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-// campo email
-function validateFields() {
-    const emailValid = isEmailValid();
-    document.getElementById("recover-password-button").disabled = !emailValid;
-
-    const passwordValid = isPasswordValid();
-    document.getElementById('login-button').disabled = !email || !passwordValid
+// campo email e password
+function onChangeEmail() {
+    emailErros();
+    buttonsDisable();
+}
+function onChangePassword() {
+    passwordErro();
+    buttonsDisable();
 }
 function isEmailValid() {
     const email = document.getElementById("email").value;
@@ -12,6 +13,43 @@ function isEmailValid() {
         return false;
     }
     return validateEmail(email);
+    
+}
+//função criada expor mensagens de erros.
+function emailErros() {
+    const email = document.getElementById('email').value;
+    if (!email) {
+        document.getElementById('email-required-error').style.display = "block";
+    } else {
+        document.getElementById('email-required-error').style.display = "none";
+
+    }
+    if (validateEmail(email)) {
+        document.getElementById('email-invalid-error').style.display = "none";
+        
+    } else {
+        document.getElementById('email-invalid-error').style.display = "block";
+    }
+    
+}
+function passwordErro() {
+    const password = document.getElementById('password').value;
+    if (!password) {
+        document.getElementById("password-required-erro").style.display = "block";
+        
+    } else {
+        document.getElementById('password-required-erro').style.display = "none";
+    }
+        
+    
+    
+}
+function buttonsDisable() {
+    const emailValid = isEmailValid();
+    document.getElementById("recover-password-button").disabled = !emailValid;
+
+    const passwordValid = isPasswordValid();
+    document.getElementById('login-button').disabled = !email || !passwordValid
     
 }
 //função criada para retorno se é falso ou verdadeiro para a validade da senha.

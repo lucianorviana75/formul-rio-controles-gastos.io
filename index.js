b64b9eadd72edf50849aad9affddb2a045845297
luciano <ribeirovianaluciano98@gmail.com>
2024-09-13 10:42:46 -0300

    2°alteração na area do email

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
+// campo email
 function validateFields() {
-    //pegar o valor do campo de email
-    const emaiValid = isEmailValid();
-    document.getElementById('recover-password-button').disabled = !emaiValid;
-    const password = isEmailValid();
-    document.getElementById('login-button').disabled = !emaiValid || !isPasswordValid;
-    /*
     const email = document.getElementById("email").value;
     if (!email) {
-        document.getElementById('recover-password-button').disabled = true;
-    } else if (validateEmail(email)) {
-        document.getElementById('recover-password-button').disabled = false;
-    } else {
-        document.getElementById('recover-password-button').disabled = true;
+        document.getElementById("recover-password-button").disabled = true;
     }
-    */
-    //verificar se o email não é vazio e o email é válido
-    //se verdadeiro, então habilitar o botão de recuperar senha
-    //se falso, então desabilitar o botão de recuperar senha
+    else if (validateEmail(email)) {
+        document.getElementById("recover-password-button").disabled = false;
+    }
+    else {
+        document.getElementById("recover-password-button").disabled = true;
+    }
+
+        
+    
 }
 function isEmailValid() {
     const email = document.getElementById("email").value;
@@ -24,13 +20,7 @@ function isEmailValid() {
         return false;
     }
     return validateEmail(email);
-}
-function isPasswordValid() {
-    const password = document.getElementById("password").value;
-    if (!password) {
-        return false;
-    }
-    return true;
+    
 }
 function validateEmail(email) {
     return /\S+@\S+\.\S+/.test(email);

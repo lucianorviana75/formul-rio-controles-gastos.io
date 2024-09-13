2ea24436071939e45646f2b9ae675b4da2514929
luciano <ribeirovianaluciano98@gmail.com>
2024-09-13 15:07:03 -0300

    Etapa 2° completos 'botões funcionando com javaScript'

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 function validateFields() {
     const emailValid = isEmailValid();
     document.getElementById("recover-password-button").disabled = !emailValid;
+
+    const passwordValid = isPasswordValid();
+    document.getElementById('login-button').disabled = !email || !passwordValid
 }
 function isEmailValid() {
     const email = document.getElementById("email").value;
@@ -10,6 +13,14 @@ function isEmailValid() {
     }
     return validateEmail(email);
     
+}
+//função criada para retorno se é falso ou verdadeiro para a validade da senha.
+function isPasswordValid() {
+    const password = document.getElementById("password").value;
+    if (!password) {
+        return false;
+    }
+    return true;
 }
 function validateEmail(email) {
     return /\S+@\S+\.\S+/.test(email);
